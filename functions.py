e7220e500f358b0cf906a39026a4398ce735cbcf
Sylvain <60986961+Sylvain-Deposit@users.noreply.github.com>
2022-08-03 17:36:25 +0100

    Added DrawImages, comments, etc...

--- functions.py
+++ functions.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May 13 16:19:24 2022
-
-@author: Sylvain
-
 Functions are used to weight the sizes, alphas, etc... of the plots.
 They have to return a single array normalized betwen 0 and 1.
 """
 
-
 import numpy as np
 
-#%% new cell
+
 class Distance:
+    """This class will output values between 0 and 1 depending on the distance 
+    of the point from the centre of the distribution.
+    """
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -27,20 +26,20 @@ class Distance:
         return (r - r.min()) / (r.max() - r.min())
 
     def inc_uniform(self):
+        # Simple increasing ramp.
         return np.sqrt(self.x ** 2 + self.y ** 2)
 
     def normal(self, sd=0.4):
-
+        # Centered gaudssian distribution.
         _d = self.inc_uniform()
-
         gaussian_weight = (
             1 / (sd * np.sqrt(2 * np.pi)) * np.exp(-(_d ** 2) / (2 * sd ** 2))
         )
 
-        return gaussian_weight
+        return self._normalize(gaussian_weight)
 
     def wave(self, waves=3):
-
+        # Centered wave pattern
         _d = self.inc_uniform() * waves * 2 * np.pi
 
         w = np.cos(_d)
@@ -50,7 +49,7 @@ class Distance:
         return w
 
     def laplace(self):
-
+        # Centered Laplace distribution
         _d = self.inc_uniform()
         w = 0.5 * np.exp(-_d)
         w = self._normalize(w)
@@ -59,6 +58,10 @@ class Distance:
 
 
 class Angle:
+    """This class will output weights between 0 and 1, depending on the theta 
+    angle of the point.
+    """
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -70,19 +73,14 @@ class Angle:
             return
 
     def _cart_to_polar(self, x, y):
-
-        # r = np.sqrt(x**2 + y**2)
-        # theta = np.arctan(y / x)
-        
-        r = np.sqrt(x**2 + y**2)
-
-        theta = np.where(y >= 0, np.arccos(x/r), -np.arccos(x/r))
-        # theta = np.arctan(y / x)
-        
+        # Convert cartesian coordinates to polar.
+        r = np.sqrt(x ** 2 + y ** 2)
+        theta = np.where(y >= 0, np.arccos(x / r), -np.arccos(x / r))
 
         return r, theta
 
     def wave(self, waves=2):
+        # waves depending on the theta angle.
         r, theta = self._cart_to_polar(self.x, self.y)
         theta = theta * waves
 
@@ -90,3 +88,18 @@ class Angle:
         value = np.nan_to_num(value, nan=0)
 
         return value
+
+
+class Value:
+    """This class outputs a normalized version of the input array. Used to index
+    an attribute to any array.
+    """
+
+    def __init__(self, x):
+        self.x = x
+
+    def _normalize(self, r):
+        return (r - r.min()) / (r.max() - r.min())
+
+    def as_index(self):
+        return self._normalize(self.x)
--- piles.py
+++ piles.py
@@ -15,10 +15,11 @@ from helpers import color_dict, polygon_dict
 from PIL import Image
 from PIL.ImageDraw import ImageDraw, _compute_regular_polygon_vertices
 
+
 @dataclass
 class PILe:
-    
-    ''' Basic data structure for PILes distributions.
+
+    """ Basic data structure for PILes distributions.
     Parameters available:
         * coords: 2-tuple of np.ndarray(), x and y coordinates of the distribution.
         The distribution centre (0, 0) is at the centre of the image.
@@ -27,6 +28,9 @@ class PILe:
         * shapes: str or list of str, name of the shapes to draw at the coordinates x, y.
         Default:
             'circle'
+        * images: Image to be pasted at every (x, y) coordinate when using DrawImages.
+        Default:
+            PIL Image of 10*10 pixels, transparent.
         * sizes: int or list of int, sizes of the shapes to draw, in number of pixels.
         Default: 
             20
@@ -63,37 +67,39 @@ class PILe:
         Default values:
             height = 2
             width = 2
-    '''
-    
+    """
+
     coords = (np.asarray([0]), np.asarray([0]))
     height = 2
     width = 2
-    shapes = 'circle'
-    
+    shapes = "circle"
+    images = Image.new("RGBA", (10, 10), (255, 255, 255, 0))
     sizes = 20
     alphas = 255
     colors = (255, 0, 0)
     outlines = (0, 0, 0)
     widths = 3
     angles = 0
-    ratios = 1 
+    ratios = 1
+
 
 # Regex for parsing the names of n-gons.
-# Will match '3-gon', '4-gon', etc...     
+# Will match '3-gon', '4-gon', etc...
 n_gons_patterns = re.compile("^\d+-gon$")
 
 
 class ImageDraws(ImageDraw):
-    ''' Main class for drawing multiple shapes with a single call.
+    """ Main class for drawing multiple shapes with a single call.
     Built on top of ImageDraw from PIL.    
     
-    '''
+    """
+
     def __init__(self, img):
         ImageDraw.__init__(self, img, mode="RGBA")
         self.img = img
 
     def _return_proper_values(self, values, n=100):
-        '''
+        """
         If a single element, converts it to a list of these elements, of length n.
         If a numpy array, coerce it to length n.
         Parameters
@@ -107,22 +113,19 @@ class ImageDraws(ImageDraw):
         -------
         values : list or np.asarray() of length n
 
-        '''
+        """
 
         if isinstance(values, np.ndarray):
             if values.size < n:
                 return np.resize(values, n)
-
         if not isinstance(values, (list, np.ndarray)):
             values = [values]
-
         if len(values) < n:
             values = values * (n // len(values))
-
         return values
 
     def _return_proper_color(self, color, alpha):
-        '''
+        """
         Check if the input color matches (R, G, B) format or is a known color string.
         And combines the value with alpha.
         
@@ -138,7 +141,7 @@ class ImageDraws(ImageDraw):
         fill_color : tuple (R, G, B, A) of int8
             Final color value.
 
-        '''
+        """
 
         if isinstance(color, str):
             color = color_dict[color.upper()]
@@ -151,73 +154,93 @@ class ImageDraws(ImageDraw):
                 fill_color = color
         return fill_color
 
-    def _generic_drawer(self, shape, x, y, size, fill_color, outline, width, angle, ratio):
+    def _generic_drawer(
+        self, shape, x, y, size, fill_color, outline, width, angle, ratio
+    ):
         # Will be used for circles, rectangles, ellipses, squares.
         # PIL does not manage alpha channel correctly when drawing a shape.
         # We have to draw the shape on another image and use alpha_composite to paste this temp image.
-        
+
         # We have to manage the width in the image size, as it is an external width:
         # a circle of radius 10 with a line width of 1 has a full radius of 11.
-        tmp = Image.new("RGBA", (int(size*2*ratio + width*2+1), int(size*2 + width*2+1)),
-                        (255, 255, 255, 0))
+        tmp = Image.new(
+            "RGBA",
+            (int(size * 2 * ratio + width * 2 + 1), int(size * 2 + width * 2 + 1)),
+            (255, 255, 255, 0),
+        )
         tmp_draw = ImageDraw(tmp)
         # Simply get the method from the class and use it.
         getattr(tmp_draw, shape)(
-            (0, 0, int(size*2*ratio + width), int(size*2 + width)), fill=fill_color, outline=outline, width=width)
+            (0, 0, int(size * 2 * ratio + width), int(size * 2 + width)),
+            fill=fill_color,
+            outline=outline,
+            width=width,
+        )
 
         # The generic drawer does not manage rotations, adding it here.
         if angle != 0:
             tmp = tmp.rotate(angle, expand=True, resample=Image.BICUBIC)
-
         # Correcting for the centre.
         tmp_x, tmp_y = tmp.size
         tmp_x /= 2
         tmp_y /= 2
 
         # And pasting with alpha.
-        self.img.alpha_composite(tmp, (int(x-tmp_x + width/2), int(y-tmp_y + width/2)))
-        
+        self.img.alpha_composite(
+            tmp, (int(x - tmp_x + width / 2), int(y - tmp_y + width / 2))
+        )
+
     def regular_polygon(
         self, bounding_circle, n_sides, rotation=0, fill=None, outline=None, width=1
     ):
         """I overclassed the original PIL function, as it does not allow different line widths"""
         xy = _compute_regular_polygon_vertices(bounding_circle, n_sides, rotation)
         self.polygon(xy, fill, outline, width)
-        
-        
-    def _draw_polygon(self, x, y, size, n_sides, fill_color, outline, width, angle, ratio):
-        
+
+    def _draw_polygon(
+        self, x, y, size, n_sides, fill_color, outline, width, angle, ratio
+    ):
+
         # Funny one: rectangle & ellipse drawers are defined by their bounding boxes
         # but polygon drawer is defined by centre and radius of the inscribed circle.
         # So a square is drawn 1.424 bigger than a 4-gon. Correcting this here.
-        
+
         size *= 1.424
-        
-        tmp = Image.new("RGBA", (int(size*2*ratio + width), int(size*2 + width)),
-                        (255, 255, 255, 0))
+
+        tmp = Image.new(
+            "RGBA",
+            (int(size * 2 * ratio + width), int(size * 2 + width)),
+            (255, 255, 255, 0),
+        )
         tmp_draw = ImageDraws(tmp)
-        
+
         tmp_x, tmp_y = tmp.size
         tmp_x /= 2
         tmp_y /= 2
         # The drawer manages rotations by itself, no need to add it.
-        tmp_draw.regular_polygon((tmp_x, tmp_y, size), n_sides, rotation=angle, fill=fill_color, outline=outline, width=width)
-       
+        tmp_draw.regular_polygon(
+            (tmp_x, tmp_y, size),
+            n_sides,
+            rotation=angle,
+            fill=fill_color,
+            outline=outline,
+            width=width,
+        )
+
         # And pasting with alpha.
-        self.img.alpha_composite(tmp, (int(x-tmp_x + width/2), int(y-tmp_y + width/2)))
-        
+        self.img.alpha_composite(
+            tmp, (int(x - tmp_x + width / 2), int(y - tmp_y + width / 2))
+        )
 
     def DrawShapes(self, params=PILe):
         # Main method to draw multiple shapes in one call.
 
-    
         # Checking if x and y are numpy arrays.
         xs, ys = params.coords
         if not isinstance(xs, np.ndarray):
             xs = np.asarray(xs)
         if not isinstance(ys, np.ndarray):
             ys = np.asarray(ys)
-            
         # Building all the arrays of parmeters with proper length.
         shapes = self._return_proper_values(params.shapes, len(xs))
         sizes = self._return_proper_values(params.sizes, len(xs))
@@ -228,92 +251,108 @@ class ImageDraws(ImageDraw):
         widths = [int(x) for x in widths]
         angles = self._return_proper_values(params.angles, len(xs))
         ratios = self._return_proper_values(params.ratios, len(xs))
-        
+
         # Scaling x & y and centering them in the image.
         xs = xs * (params.width) / 2 + self.img.width / 2
         ys = ys * (params.height) / 2 + self.img.height / 2
-        
-        all_params = zip(shapes, xs, ys, sizes, alphas, colors, outlines, widths, angles, ratios)
-        
 
-        for shape, x, y, size, alpha, fill_color, outline, width, angle, ratio in all_params:
+        all_params = zip(
+            shapes, xs, ys, sizes, alphas, colors, outlines, widths, angles, ratios
+        )
+
+        for (
+            shape,
+            x,
+            y,
+            size,
+            alpha,
+            fill_color,
+            outline,
+            width,
+            angle,
+            ratio,
+        ) in all_params:
 
             fill_color = self._return_proper_color(fill_color, alpha)
 
             outline = self._return_proper_color(outline, alpha)
-            
+
             # Basic drawers
-            if shape in ['rectangle', 'ellipse']:
+            if shape in ["rectangle", "ellipse"]:
                 self._generic_drawer(
-                    shape, x, y, size, fill_color, outline, width, angle, ratio)
-                
-            if shape == 'circle':
-                shape = 'ellipse'
+                    shape, x, y, size, fill_color, outline, width, angle, ratio
+                )
+            if shape == "circle":
+                shape = "ellipse"
                 ratio = 1
                 angle = 0
                 self._generic_drawer(
-                    shape, x, y, size, fill_color, outline, width, angle, ratio)
- 
+                    shape, x, y, size, fill_color, outline, width, angle, ratio
+                )
             # Shortcuts if you don't want to call a triangle '3-gon'.
             if shape in polygon_dict:
                 n_sides = polygon_dict[shape]
                 ratio = 1
-                
-                self._draw_polygon(x, y, size, n_sides, fill_color, outline, width, angle, ratio)
-                
+
+                self._draw_polygon(
+                    x, y, size, n_sides, fill_color, outline, width, angle, ratio
+                )
             if n_gons_patterns.match(shape):
-                n_sides = int(shape.split('-')[0])
+                n_sides = int(shape.split("-")[0])
                 ratio = 1
 
-                self._draw_polygon(x, y, size, n_sides, fill_color, outline, width, angle, ratio)
-                
+                self._draw_polygon(
+                    x, y, size, n_sides, fill_color, outline, width, angle, ratio
+                )
 
     def DrawLines(self, params=PILe, closed=False):
-        # Same as DrawShapes, but for lines.       
+        # Same as DrawShapes, but for lines.
 
         xs, ys = params.coords
-        
+
         if not isinstance(xs, np.ndarray):
             xs = np.asarray(xs)
         if not isinstance(ys, np.ndarray):
             ys = np.asarray(ys)
-            
         outlines = self._return_proper_values(params.outlines, len(xs))
         widths = self._return_proper_values(params.widths, len(xs))
         widths = [int(x) for x in widths]
-        
+
         alphas = self._return_proper_values(params.alphas, len(xs))
         alphas = [int(x) for x in alphas]
-        
+
         xs = xs * (params.width) / 2 + self.img.width / 2
         ys = ys * (params.height) / 2 + self.img.height / 2
-        
-        # We may want to close the figure, thus drawing a line between 
+
+        # We may want to close the figure, thus drawing a line between
         # the last and the first point.
         if closed:
             xs = np.append(xs, xs[0])
             ys = np.append(ys, ys[0])
-   
-        for i in range(len(xs)-1):
-            
-            x1, x2 = xs[i], xs[i+1]
-            y1, y2 = ys[i], ys[i+1]
-            
+        for i in range(len(xs) - 1):
+
+            x1, x2 = xs[i], xs[i + 1]
+            y1, y2 = ys[i], ys[i + 1]
+
             width = widths[i]
             outline = outlines[i]
             alpha = alphas[i]
             outline = self._return_proper_color(outline, alpha)
-            
-            tmp_width = abs(x1 - x2) + 2*width
-            tmp_height = abs(y1 - y2) + 2*width
 
-            tmp = Image.new("RGBA", (int(tmp_width), int(tmp_height)),
-                            (255, 255, 255, 0))
+            tmp_width = abs(x1 - x2) + 2 * width
+            tmp_height = abs(y1 - y2) + 2 * width
+
+            tmp = Image.new(
+                "RGBA", (int(tmp_width), int(tmp_height)), (255, 255, 255, 0)
+            )
             tmp_draw = ImageDraw(tmp)
 
-            tmp_draw.line((width, width, tmp_width-width, tmp_height-width),
-                          fill=outline, width=width)
-            
+            tmp_draw.line(
+                (width, width, tmp_width - width, tmp_height - width),
+                fill=outline,
+                width=width,
+            )
+
             # Annoying thing to draw lines in any direction and rotate them in any angle.
             # Instead of computing the angle, I flip the image if needed.
             corner_x, corner_y = x1 - width, y1 - width
@@ -321,10 +360,49 @@ class ImageDraws(ImageDraw):
             if x1 > x2:
                 tmp = tmp.transpose(Image.FLIP_LEFT_RIGHT)
                 corner_x = x2 - width
-
             if y1 > y2:
                 tmp = tmp.transpose(Image.FLIP_TOP_BOTTOM)
                 corner_y = y2 - width
-
             self.img.alpha_composite(tmp, dest=(int(corner_x), int(corner_y)))
 
+    def DrawImages(self, params=PILe):
+        # Main method to draw multiple images in one call.
+
+        # Checking if x and y are numpy arrays.
+        xs, ys = params.coords
+        if not isinstance(xs, np.ndarray):
+            xs = np.asarray(xs)
+        if not isinstance(ys, np.ndarray):
+            ys = np.asarray(ys)
+        imgs = params.images
+
+        # Putting images in a proper list
+        if not isinstance(imgs, (list, np.ndarray)):
+            imgs = [imgs]
+        if len(imgs) < len(xs):
+            imgs = imgs * (len(xs) // len(imgs))
+        sizes = self._return_proper_values(params.sizes, len(xs))
+        alphas = self._return_proper_values(params.alphas, len(xs))
+        angles = self._return_proper_values(params.angles, len(xs))
+        ratios = self._return_proper_values(params.ratios, len(xs))
+
+        # Scaling x & y and centering them in the image.
+        xs = xs * (params.width) / 2 + self.img.width / 2
+        ys = ys * (params.height) / 2 + self.img.height / 2
+
+        all_params = zip(imgs, xs, ys, sizes, alphas, angles, ratios)
+
+        for img2, x, y, size, alpha, angle, ratio in all_params:
+
+            if angle != 0:
+                img2 = img2.rotate(angle, expand=True, resample=Image.BICUBIC)
+            if alpha != 255:
+                img3 = img2.copy()
+                img3.putalpha(alpha)
+                img2.paste(img3, img2)
+            tmp_x, tmp_y = img2.size
+            tmp_x /= 2
+            tmp_y /= 2
+
+            # And pasting with alpha.
+            self.img.alpha_composite(img2, (int(x - tmp_x), int(y - tmp_y)))
