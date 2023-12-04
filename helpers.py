a44b0b3409c2d91955277dac5aa8e0c2107e5e8e
Sylvain <60986961+Sylvain-Deposit@users.noreply.github.com>
2023-12-04 11:40:21 +0000

    Added RGB <-> CieLAB

--- helpers.py
+++ helpers.py
@@ -9,6 +9,25 @@ from PIL import Image
 from PIL.ImageDraw import ImageDraw, ImageFont
 import numpy as np
 
+from PIL import ImageCms
+
+class ColorTrans:
+
+    '''Class for transforming RGB<->LAB color spaces for PIL images.
+    Taken from https://stackoverflow.com/a/65450572/13461740 '''
+    
+    def __init__(self):
+        self.srgb_p = ImageCms.createProfile("sRGB")
+        self.lab_p  = ImageCms.createProfile("LAB")
+        self.rgb2lab_trans = ImageCms.buildTransformFromOpenProfiles(self.srgb_p, self.lab_p, "RGB", "LAB")
+        self.lab2rgb_trans = ImageCms.buildTransformFromOpenProfiles(self.lab_p, self.srgb_p, "LAB", "RGB")
+    
+    def rgb2lab(self, img):
+        return ImageCms.applyTransform(img, self.rgb2lab_trans)
+
+    def lab2rgb(self, img):
+        return ImageCms.applyTransform(img, self.lab2rgb_trans)
+
 # Simple dict to match the name of the shape with the corresponding number of sides.
 polygon_dict = {
     "triangle": 3,
